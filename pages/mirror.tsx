2720d3b2a30f6365c59656f10152e4ec3571a6d5
Yahas Salankimatt <yashas.salankimatt@gmail.com>
2025-07-16 00:04:05 -0700

    Added mirror route

---
+++ pages/mirror.tsx
@@ -0,0 +1,16 @@
+import { useRouter } from "next/router";
+import React, { useEffect } from "react";
+
+function proprio(props) {
+	const router = useRouter();
+
+	useEffect(() => {
+		router.push(
+			"https://www.notion.so/proprio/Mirror-Rewiring-Your-Brain-for-Peak-Productivity-and-Happiness-2329618527ed80abb0c5d0af794be906?source=copy_link"
+		);
+	}, []);
+
+	return <div></div>;
+}
+
+export default proprio;
